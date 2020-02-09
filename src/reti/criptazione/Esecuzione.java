28ee5c27c08e041790a3a272cf3dfdf9102a84c6
kira12446 <60788633+kira12446@users.noreply.github.com>
2020-02-09 11:50:53 +0100

    gigi

--- src/reti/criptazione/Esecuzione.java
+++ src/reti/criptazione/Esecuzione.java
@@ -6,18 +6,53 @@ public class Esecuzione {
 
 	//private Structure str = Structure.generateStructure();
 	private static Scanner scan;
+	private int n;
+	private AlgorithmsFunctions function;
 
 	public static void main(String[] args) {
 		//Structure s = Structure.generateStructure();
-	    System.out.println("Inserisci il messaggio da criptare:");
+		Esecuzione E = new Esecuzione();
+		E.start();
+	    
+	}
+	
+	public void start() {
+		
+		System.out.println("Inserisci il messaggio da criptare:");
 	    scan = new Scanner(System.in);
 	    String in = scan.nextLine();
 	    Message M = new Message();
 	    M.setMessage(in);
+	    System.out.println("Select the function to apply to the message:");
+	    in = scan.nextLine();
+	    selectFunction(in);
 	    AlgorithmsFunctions add = new Add(4);
 	    Algorithms A = new AlgotithmEvenIteration(M, add);
 	    A.iteration();
 	    System.out.print(M.getListMessage());
+		
+	}
+	public void info() {
+		System.out.println("You can enter one of the following commands:");
 	}
 
+	public void selectFunction(String in) {
+		if(in.equalsIgnoreCase("Add")) {
+			System.out.println("Select the number (integer) to be added:");
+			int input = scan.nextInt();
+			n = input;
+			function = new Add(n);
+		}else if (in.equalsIgnoreCase("Multiply")) {
+			System.out.println("Select the number (integer) to be multiply:");
+			int input = scan.nextInt();
+			n = input;
+			function = new Multiply(n);
+		}else if(in.equalsIgnoreCase("info")) {
+			info();
+		}else {
+			System.out.println("The function you have entered is not present in the encryption system.");
+			info();
+			selectFunction(in);
+		}
+	}
 }
